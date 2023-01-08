ceb46f2885c0f69104d86bb2bbdc56e51336aabf
Zeke Abshire <zekeabshire@gmail.com>
2023-01-08 05:20:29 -0600

    added all assets and typography styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
+    content: ["./src/**/*.{js,jsx,ts,tsx}"],
+    theme: {
+        extend: {
+            colors: {
+                "nord-black": "#4C566A",
+                "nord-grey": "#ECEFF4",
+                "nord-white": "#FFFFFF",
+                "nord-purple": "#B48EAD",
+            },
+        },
+        fontFamily: {
+            "zen-maru": ["Zen Maru Gothic", "sans-serif"],
+        },
+    },
+    plugins: [],
+};
