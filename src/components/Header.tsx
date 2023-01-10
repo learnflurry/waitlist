01186106abdb8742674c051812da4830e0bf69fe
Zeke Abshire <zekeabshire@gmail.com>
2023-01-09 21:39:22 -0600

    finished desktop header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,42 @@
 import { ReactComponent as FlurryLogo } from "assets/flu-small.svg";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+    const [isPaddingLarge, setIsPaddingLarge] = useState(true);
+
+    // Scroll Listener
+    useEffect(() => {
+        const listenScrollEvent = () => {
+            const threshold = 10;
+            setIsPaddingLarge(
+                document.body.scrollTop < threshold &&
+                    document.documentElement.scrollTop < threshold
+            );
+        };
+
+        window.addEventListener("scroll", listenScrollEvent);
+        return () => {
+            window.removeEventListener("scroll", listenScrollEvent);
+        };
+    });
+
     return (
-        <>
-            <FlurryLogo />
-            <h3>Flurry</h3>
-            <a href="#">About</a>
-            <a href="#">FAQ</a>
-            <a href="#">Roadmap</a>
-        </>
+        <nav
+            className={`sticky top-0 w-full backdrop-blur-lg backdrop-filter transition-all duration-300 ease-in-out ${
+                isPaddingLarge ? "py-8" : "py-4 shadow"
+            }`}
+        >
+            <div className="container flex flex-row justify-between ">
+                <a href="#intro" className="flex flex-row gap-x-3">
+                    <FlurryLogo />
+                    <h3>Flurry</h3>
+                </a>
+                <div className="flex flex-row items-center gap-x-16">
+                    <a href="#about">About</a>
+                    <a href="#faq">FAQ</a>
+                    <a href="#roadmap">Roadmap</a>
+                </div>
+            </div>
+        </nav>
     );
 }
